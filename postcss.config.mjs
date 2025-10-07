c3b5df1fc6b2e0b9f71ccfab8e280ecbe63dfea6
gmena82 <130086132+gmena82@users.noreply.github.com>
2025-10-06 20:22:56 -0500

    Fix PostCSS plugin configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+const config = {
+  plugins: [tailwindcss, autoprefixer]
 };
+
+export default config;
